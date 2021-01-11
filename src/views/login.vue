8ea4a12a71854e4e160b09a6d3273a33e05c8e61
2018top <184383@qq.com>
2021-01-11 13:18:44 +0800

    init

---
+++ src/views/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login">
+        
+        <com-login></com-login>
+    </div>
+</template>
+<script>
+import comLogin from '@/components/com-login.vue'
+
+
+import { Button, Select } from 'element-ui';
+export default {
+   
+    data:function(){
+        return{
+            msg:"你好世界"
+        }
+    },
+    components:{
+        comLogin
+    }
+}
+</script>
---
+++ src/views/welcome.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="welcome">
+      个人中心管理页
+    <el-container>
+      <el-aside width="200px" >侧边导航</el-aside>
+      <el-container>
+        <el-header style="background:bisque">头部</el-header>
+        <el-main style="background:cadetblue">内容</el-main>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+<script>
+export default {
+    name: 'Container',
+ 
+}
+</script>
+<style lang="less" scoped>
+.welcome{
+    .el-aside{
+        background: crimson;
+    }
+}
+</style>
